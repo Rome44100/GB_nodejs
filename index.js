a88f3fe6fc73d624364d5720213ebdf001bb13e1
Rome44100 <rome44100@gmail.com>
2021-09-26 18:43:08 +0300

    Start Lesson 1. Complete exercise 1.

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-const colors = require("colors/safe");
+// EXERCISE 1
 
-let start  = 0;
-let end    = 10;
-let result = [];
-let simple = true;
+// console.log('Record 1');
 
-if(process.argv[2] != undefined && process.argv[3] != undefined) {
-    start = parseInt(process.argv[2], 10);
-    end = parseInt(process.argv[3], 10);
-    if(isNaN(start) || isNaN(end)) {
-        console.log(colors.red("One of the args (or both of them) is not a digit! Exit!"));
-        process.exit();
-    }
-} else {
-    console.log(colors.red("One of the args (or both of them) is empty! Exit!"));
-    process.exit();
-}
+// setTimeout(() => {
+//   console.log('Record 2');
+//   Promise.resolve().then(() => {
+//     setTimeout(() => {
+//         console.log('Record 3');
+//         Promise.resolve().then(() => {
+//             console.log('Record 4');
+//         });
+//     });
+//   });       
+// });
 
-if(start <= 1) start = 2;
+// console.log('Record 5');
 
-for(let i = start; i <= end; i++) {
-    for(let j = 2; j < i; j++) {
-        if(i % j === 0) {
-            simple = false;
-        }
-    }
-    if(simple) {
-        result.push(i);
-    }
-    simple = true;
-}
+// Promise.resolve()
+//     .then(() => Promise.resolve()
+//         .then(() => console.log('Record 6')));
 
-if(result.length === 0) {
-    console.log(colors.red("There are no simple digits!"));
-} else {
-    for(let n = 0; n < result.length; n++) {
-        if(n % 3 === 0) console.log(colors.green(result[n]));
-        if((n + 2) % 3 === 0) console.log(colors.yellow(result[n]));
-        if((n + 1) % 3 === 0) console.log(colors.red(result[n]));
-    }
-}
+// 1 5 6 2 3 4
+
+// EXERCISE 2
+
