5a295e69f8a4d965db5a4c1925b842540d8a2dab
Rome44100 <rome44100@gmail.com>
2021-09-28 19:08:55 +0300

    Complete Lesson 2. Complete exercise 2.

--- index.js
+++ index.js
@@ -24,37 +24,49 @@
 // 1 5 6 2 3 4
 
 // EXERCISE 2
+const { parse } = require('date-fns');
+let format = require('date-fns/format');
+var isFuture = require('date-fns/isFuture');
+var compareDesc = require('date-fns/compareDesc');
+var formatDistanceToNowStrict = require('date-fns/formatDistanceToNowStrict');
+var intervalToDuration = require('date-fns/intervalToDuration');
 
 const EventEmitter = require("events");
+class MyEmitter extends EventEmitter {};
+const emitter = new MyEmitter();
 
-function TimeInterval(param) {
-    const args = param;
+// add minute because its imposible to test
+let pattern = "mm-HH-dd-MM-yyyy";
+const today = new Date();
+const now = format(today, pattern);
+let future = today.getFullYear();
+
+// user input validation parse date-like string
+if(parse(process.argv[2], pattern, today) == "Invalid Date") {
+    console.log("Enter date like", pattern);
+    process.exit();
+}
+future = parse(process.argv[2], pattern, today);
+
+// validate future date
+if(!isFuture(future)) {
+    console.log("Enter date more than now", now);
+    process.exit();
+}
+
+function tick() {
     const id = setInterval(() => {
+        const start = new Date();
+        const compare = compareDesc(start, future);
         console.clear();
-        console.log("Remaind =", param--);
-        if(param == 95) {
-            console.clear();
-            console.log("Remaind =", param);
+        // console.log("Remind", formatDistanceToNowStrict(future));
+        console.log("Remind", intervalToDuration({ start: start, end: future }));
+        if (compare != 1) {
             console.log("End timer! Goodbye!");
             clearInterval(id);
         }
     }, 1000);
 }
 
-const run = async (param) => {
-    // emitter.emit("tickjj", "remaind"); // ???????
-    await new Promise(resolve => {
-        TimeInterval(param);
-    });
-    await run(param);
-}
-
-function tick(payload) {
-    console.log("Remaind =", payload);
-}
-
-const emitter = new EventEmitter();
-
 emitter.on("tick", tick);
-
-run(100);
+emitter.emit("tick");
